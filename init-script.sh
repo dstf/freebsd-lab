09a08494a93fe5d2adb6b7a7315b7f147f18fd47
Davide staffieri <davide@protoweb.me>
2022-01-12 12:07:12 +0000

    Update init-script.sh

--- init-script.sh
+++ init-script.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 pkg update && pkg upgrade
+pkg install nginx
+echo 'nginx_enable="YES"' >> /etc/rc.conf
+service nginx start
+sysrc firewall_enable="YES"
+sysrc firewall_quiet="YES"
+sysrc firewall_myservices="22/tcp 80/tcp 443/tcp"
